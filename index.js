f378316e09a4d81b29e99c93941a2047df31df94
JUAN <juanbeta00@hotmail.com>
2022-05-18 11:51:06 -0400

    commit tree 4671fb46d0826cb9b97a793fbe53e2cc16429794

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = "Juan B.";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase()+ "!";
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+
